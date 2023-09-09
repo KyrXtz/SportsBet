9b3528cee1352a843056cf133cf3757b31017614
Kyriakos Ch <33755183+KyrXtz@users.noreply.github.com>
2023-09-09 18:26:07 +0300

    Delete Infrastructure2 directory

--- Infrastructure2/Configuration/LoggingConfiguration.cs
+++
@@ -1,9 +0,0 @@
-namespace SportsBet.Infrastructure.Configuration;
-
-public static class LoggingConfiguration
-{
-    public static IApplicationBuilder UseLoggingHandling(this IApplicationBuilder app)
-    {
-        return app.UseMiddleware<LoggingMiddleware>();
-    }
-}
